0d6efc69fd7b7722ff3a9ff2defc0a8646bd5852
Sergiu Cimpoi <sergiu.cimpoi@gmail.com>
2025-09-14 22:38:29 +0200

    just another test

--- test/InferSpec.hs
+++ test/InferSpec.hs
@@ -5,8 +5,8 @@ import Infer
 
 import Test.Hspec
 
-spec :: Spec
-spec = describe "digitsToCellIds" $ do
+spec1 :: Spec
+spec1 = describe "digitsToCellIds" $ do
     it "assigns indices to candidates correctly" $ do
         let input =
                 [ Candidates [1, 2]
@@ -17,14 +17,47 @@ spec = describe "digitsToCellIds" $ do
                 ]
             expected =
                 V.fromList
-                    [ [0] -- candidate 1 appears at cell 0
-                    , [2, 0] -- candidate 2 appears at cells 2 and 0
-                    , [2] -- candidate 3 at cell 2
-                    , [] -- candidate 4 nowhere as candidate
-                    , [] -- candidate 5 nowhere as candidate
-                    , [] -- candidate 6 nowhere
-                    , [] -- candidate 7 nowhere
-                    , [] -- candidate 8 nowhere
-                    , [3] -- candidate 9 at cell 3
+                    [ [0]
+                    , [2, 0]
+                    , [2]
+                    , []
+                    , []
+                    , []
+                    , []
+                    , []
+                    , [3]
                     ]
         digitsToCellIds input `shouldBe` expected
+
+spec2 :: Spec
+spec2 = describe "reduceHiddenPairs" $ do
+    it "reduction of hidden pairs" $ do
+        let input =
+                V.fromList
+                    [ Candidates [3, 8]
+                    , Value 2
+                    , Candidates [3, 6]
+                    , Candidates [3, 4, 6]
+                    , Value 5
+                    , Candidates [3, 4]
+                    , Candidates [1, 7, 8]
+                    , Value 9
+                    , Candidates [1, 6, 7]
+                    ]
+            expected =
+                V.fromList
+                    [ Candidates [3, 8]
+                    , Value 2
+                    , Candidates [3, 6]
+                    , Candidates [3, 4, 6]
+                    , Value 5
+                    , Candidates [3, 4]
+                    , Candidates [1, 7]
+                    , Value 9
+                    , Candidates [1, 7]
+                    ]
+        reduceHiddenPairs input `shouldBe` expected
+spec :: Spec
+spec = do
+    spec1
+    spec2
